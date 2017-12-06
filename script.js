e42449231f3d79c31b9e0fd808d1352d2eee9957
Unknown <justinkjetton@gmail.com>
2017-12-05 20:47:47 -0600

    Spaces

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ function printResults(){
   var userText = document.getElementById('userInput').value;
   var totalWords = 0;
   var totalSentences = 0;
+  var totalSpaces = 0;
   totalWords = totalWords + 1
   for (var i = 0; i <userText.length; i++){
     if (userText[i] === ' '){
       totalWords++;
+      totalSpaces++;
     }
     else if (userText[i] === '.' || userText[i] === '?' || userText[i] === '!'){
       totalSentences++;
     }
         document.getElementById("userResults").innerHTML="There are " + totalWords + " words in this text.";
         document.getElementById("totalSentences").innerHTML="There are " +totalSentences + " sentences in this text.";
+        document.getElementById("totalSpaces").innerHTML="There are " + totalSpaces + " spaces in this text.";
 }}
